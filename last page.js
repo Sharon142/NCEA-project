3cb5cb167870a8408ad667f1bf893a3ad3d32360
Sharon142 <culas.s@maristcollege.school.nz>
2021-06-01 13:22:33 +1200

    adding code for imput and output

--- last page.js
+++ last page.js
@@ -1,7 +1,7 @@
 function myFunction() {
     document.body.style.backgroundColor = "red";
     let input = document.querySelector("#response");
-    
+    input.innerHTML = "text"
     let output = document.querySelector("#myPara");
     output.innerHTML = "Thank you for your feedback!";
   }
