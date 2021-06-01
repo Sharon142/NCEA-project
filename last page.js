d74cffd7cddcce2ca06a6ba273ae08acfef7e4a3
Sharon142 <culas.s@maristcollege.school.nz>
2021-06-01 13:18:21 +1200

    Producing output after receiving input

--- last page.js
+++ last page.js
@@ -1,5 +1,7 @@
 function myFunction() {
     document.body.style.backgroundColor = "red";
-    let Response = document.querySelector("#response");
+    let input = document.querySelector("#response");
     
+    let output = document.querySelector("#myPara");
+    output.innerHTML = "Thank you for your feedback!";
   }
