dc5e33527403ba97738662efc3697d4dae4aa48a
Sharon142 <culas.s@maristcollege.school.nz>
2021-06-04 10:37:13 +1200

    explanation for the js coding

--- last page.js
+++ last page.js
@@ -1,7 +1,7 @@
-function myFunction() {
+function myFunction() {/*Main code for the javascript function to run*/
     document.body.style.backgroundColor = "red";/*This is for the background colour of the webpage*/
-    let input = document.querySelector("#response");
+    let input = document.querySelector("#response");/*Code used to accept responses from an end-user*/
     input.innerHTML = "text" /*The end-user's feedback is meant to be in letters*/
-    let output = document.querySelector("#myPara");
+    let output = document.querySelector("#myPara");/*code used to display the output to the end-user*/
     output.innerHTML = "Thank you for your feedback!"; /*Output after the survey is completed*/
   }
