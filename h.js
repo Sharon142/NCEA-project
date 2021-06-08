0f1fe68c8a056d2b043321271b6ca31a99ef97df
Sharon142 <culas.s@maristcollege.school.nz>
2021-06-08 14:37:34 +1200

    adding explanations

--- h.js
+++ h.js
@@ -3,7 +3,7 @@ function openNav() {
     document.getElementById("myNav").style.width = "100%";
   }
   
-  /* Navigation screen closes when someone clicks "x" */
+  /* Navigation screen closes when someone clicks "x" on the top right corner */
   function closeNav() {
     document.getElementById("myNav").style.width = "0%";
   }
