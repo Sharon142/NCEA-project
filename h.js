7ab88db2e09ad42e29d5fb44bf0b32c7df900b88
Sharon142 <culas.s@maristcollege.school.nz>
2021-06-14 10:25:59 +1200

    removing if/else statement from homepage

--- h.js
+++ h.js
@@ -8,10 +8,4 @@ function openNav() {
     document.getElementById("myNav").style.width = "0%";
   }
  
-  if (condition1) {
-    //the first if/else will be executed if the data applies to the first condition//
-  } else if (condition2) {
-    // the second if/else will be executed if the data entered applies to the second condition//
-  } else {
-    // the third if/else statement will be executed if the statement applies to the third condtion//
-  }
+  
