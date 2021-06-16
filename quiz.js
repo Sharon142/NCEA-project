e50572d714f019838312ef61c15fd90e98b0ba97
Sharon142 <culas.s@maristcollege.school.nz>
2021-06-16 12:26:44 +1200

    coding for if/else statements

---
+++ quiz.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    let Response = document.querySelector ("#response");
+    let Answer = document.querySelector("#myPara");
+    
+    if (Response.string == "Discovery of DNA") {
+        document.body.style.backgroundColor = "Green";
+        Answer.innerHTML = "You're Correct!!!";
+    } else if (Response.string == "Discovery of tobbaco mosaic virus"){
+        document.body.style.backgroundColor = "Green";
+        Answer.innerHTML = "You're Correct!!!";  
+    } else {
+        document.body.style.backgroundColor = "Red";
+        Answer.innerHTML = "Wrong answer!!!";  
+    }
+}
