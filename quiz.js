cba59ec7d13dac2bfc44a41b0d0c50f4d348fb42
Sharon142 <culas.s@maristcollege.school.nz>
2021-06-21 09:21:00 +1200

    changing variables

--- quiz.js
+++ quiz.js
@@ -1,16 +1,16 @@
 function myFunction() {
     let Response = document.querySelector ("#response");
-    let Question1 = document.querySelector("#myPara");
+    let Answer = document.querySelector("#myPara");
     
     if (Response.string == "Discovery of DNA") {
         document.body.style.backgroundColor = "Green";
-        Question1.innerHTML = "You're Correct!!!";
+        Answer.innerHTML = "You're Correct!!!";
     } else if (Response.string == "Discovery of tobbaco mosaic virus"){
         document.body.style.backgroundColor = "Green";
-        Question1.innerHTML = "You're Correct!!!";  
+        Answer.innerHTML = "You're Correct!!!";  
     } else {
         document.body.style.backgroundColor = "Red";
-        Question1.innerHTML = "Wrong answer!!!";  
+        Answer.innerHTML = "Wrong answer!!!";  
     }
        
 }
