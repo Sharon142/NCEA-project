d8bfcdb885768c24e76ebe1d6d8082cfa5af9f27
Sharon142 <culas.s@maristcollege.school.nz>
2021-06-22 14:42:33 +1200

    debugging code

--- quiz.js
+++ quiz.js
@@ -1,16 +1,16 @@
 function myFunction() {
-    let Response = document.querySelector ("#response");
-    let Answer = document.querySelector("#myPara");
+    let verdict = document.querySelector ("#response");
+    let Response = document.querySelector("#myPara");
     
-    if (Response.string = "Discovery of DNA") {
+    if (Response.string == "Discovery of DNA") {
         document.body.style.backgroundColor = "Green";
-        Answer.innerHTML = "You're Correct!!!";
-    } else if (Response.string = "Discovery of tobbaco mosaic virus"){
+        verdict.innerHTML = "You're Correct!!!";
+    } else if (Response.string == "Discovery of tobbaco mosaic virus"){
         document.body.style.backgroundColor = "Green";
-        Answer.innerHTML = "You're Correct!!!";  
+        verdict.innerHTML = "You're Correct!!!";  
     } else {
-        document.body.style.backgroundColor = "Green";
-        Answer.innerHTML = "Wrong answer!!!";  
+        document.body.style.backgroundColor = "Red";
+        verdict.innerHTML = "Wrong answer!!!";  
     }
        
 }
