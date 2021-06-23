e163b3b4b90d3f4b2e21fdd366f407c916f5f315
Sharon142 <culas.s@maristcollege.school.nz>
2021-06-24 11:44:28 +1200

    explanation of coding

--- quiz.js
+++ quiz.js
@@ -1,13 +1,13 @@
-function myFunction() {
-    let verdict = document.querySelector ("#response");
-    let Response = document.querySelector("#myPara");
-    
-    if (Response.String == "Discovery of DNA") {
-        document.body.style.backgroundColor = "Green";
-        verdict.innerHTML = "You're Correct!!!";
-    } else if (Response.String == "Discovery of tobbaco mosaic virus"){
-        document.body.style.backgroundColor = "Green";
-        verdict.innerHTML = "Correct!!!";  
+function myFunction() { //If the code is written between these brackets {} of my function, the code will run.//
+    let verdict = document.querySelector ("#response");//Response is the answer from the user and the verdict//
+    let Response = document.querySelector("#myPara");//will determine whether or not the answer is correct.//
+    //let is a type of variable code.  Response is the id of the input received by the user.//
+    if (Response.String == "Discovery of DNA") { //This is an if/else statement.  If the first condition//
+        document.body.style.backgroundColor = "Green";//is true, then the background will turn green.//
+        verdict.innerHTML = "You're Correct!!!";//This will let the user know the answer is correct.  The//
+    } else if (Response.String == "Discovery of tobbaco mosaic virus"){//second condition is which is "else if"//
+        document.body.style.backgroundColor = "Green";//accepts another answer.  The "else" staement will make the//
+        verdict.innerHTML = "Correct!!!";  //background turn red if the answer is wrong.//
     } else {
         document.body.style.backgroundColor = "Red";
         verdict.innerHTML = "Wrong answer!!!";  
@@ -24,14 +24,5 @@ function myFunction() {
     }
     let Response = document.querySelector ("#response");
     let Question3 = document.querySelector("#myPara");
-    
-    if (Response.string == "Grace Hopper") {
-        document.body.style.backgroundColor = "Green";
-        Question3.innerHTML = "You're Correct!!!";
-    } else {
-        document.body.style.backgroundColor = "Red";
-        Question3.innerHTML = "Wrong answer!!!";  
-    }
-
-       
+               
 }
