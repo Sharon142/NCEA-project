da1f4b7eb7c5519015929923997ccf3013518dbe
Sharon142 <culas.s@maristcollege.school.nz>
2021-06-24 12:04:46 +1200

    changing js data

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ function myFunction() { //If the code is written between these brackets {} of my
     let verdict = document.querySelector ("#response");//Response is the answer from the user and the verdict//
     let Response = document.querySelector("#myPara");//will determine whether or not the answer is correct.//
     //let is a type of variable code.  Response is the id of the input received by the user.//
-    if (Response.String == "Discovery of DNA") { //This is an if/else statement.  If the first condition//
+    if (Response.Text == "Discovery of DNA") { //This is an if/else statement.  If the first condition//
         document.body.style.backgroundColor = "Green";//is true, then the background will turn green.//
         verdict.innerHTML = "You're Correct!!!";//This will let the user know the answer is correct.  The//
     } else if (Response.String == "Discovery of tobbaco mosaic virus"){//second condition is which is "else if"//
