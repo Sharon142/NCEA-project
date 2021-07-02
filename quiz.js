06549b9093b587bbe9b4fda3053401ac0ad72665
Sharon142 <culas.s@maristcollege.school.nz>
2021-07-02 14:50:28 +1200

    debugging code

--- quiz.js
+++ quiz.js
@@ -6,17 +6,16 @@ function myFunction() { //If the code is written between these brackets {} of my
         if (Response.Text == "Discovery of DNA") { //This is an if/else statement.  If the first condition//
             document.body.style.backgroundColor = "Green";//is true, then the background will turn green.//
             verdict.innerHTML = "You're Correct!!!";//This will let the user know the answer is correct.  The//
-        } //second condition is which is "else if"//
-              //background turn red if the answer is wrong.//
-        } else {
+        }                                           //second condition is which is "else if"//
+                                                    //background turn red if the answer is wrong.//
+    else{
             document.body.style.backgroundColor = "Red";
             verdict.innerHTML = "Wrong answer!!!";  
         }
         
         
            
-    } 
-    
+    }    
     
        
 }
