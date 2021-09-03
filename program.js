7187f9a95e1246bab147c9c15af4e3876bfbda70
Sharon142 <culas.s@maristcollege.school.nz>
2021-09-03 18:59:41 +1200

    testing code

--- program.js
+++ program.js
@@ -2,4 +2,5 @@ function startFunction(){
 const number = ["1,2,3,4,5,6,7,8,9,10"];
 const letters = ["A,B,C,D,E,F,G,H,I,J"];
 const combine = number.concat(letters);
+document.getElementById("demo").innerHTML = combine.toString();
 }
