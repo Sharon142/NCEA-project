edcd43227c6e77d1da6797efad6f8bd3e0160ab5
Sharon142 <culas.s@maristcollege.school.nz>
2021-09-03 19:09:10 +1200

    adding loops

--- program.js
+++ program.js
@@ -3,4 +3,11 @@ const number = ["1,2,3,4,5,6,7,8,9,10"];
 const letters = ["A,B,C,D,E,F,G,H,I,J"];
 const combine = number.concat(letters);
 document.getElementById("demo").innerHTML = combine.toString();
+for (let i = 0; i < combine.length; i++) {
+    text += combine[i] + "<br>";
+  }
+}
+
+function stopFunction(){
+
 }
