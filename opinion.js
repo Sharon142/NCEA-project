460eaa54473c3d309b3dc996d30b2d728e6f2772
Sharon142 <culas.s@maristcollege.school.nz>
2021-11-07 21:32:30 +1300

    adding JS to the modal form

--- opinion.js
+++ opinion.js
@@ -2,6 +2,12 @@
 const addModal = document.querySelector('.add-modal');
 const btnAdd = document.querySelector('.btn-add');
 
+//Click add button
 btnAdd.addEventListener('click', () =>{
     addModal.classList.add('modal-show');
+});
+
+//user click anywhere outside the form
+window.addEventListener('click',  e =>{
+console.log(e.target, addModal);
 });
