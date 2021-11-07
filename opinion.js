ed11a235eebc40d650ef00254af8ba89c81595ea
Sharon142 <culas.s@maristcollege.school.nz>
2021-11-08 10:42:54 +1300

    testing code

--- opinion.js
+++ opinion.js
@@ -33,7 +33,8 @@ window.addEventListener('click',  e =>{
 });
 
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+  
+  
   const db = firebase.firestore();
  
 //Get all scientists
@@ -43,10 +44,12 @@ db.collection('user').get().then(querySnapshot => {
     })
 })
 
-"https://www.gstatic.com/firebasejs/9.2.0/firebase-app.js"
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-app.js";
  "https://firebase.google.com/docs/web/setup#available-libraries.js"
  "https://www.gstatic.com/firebasejs/9.2.0/firebase-firestore.js"
 // Your web app's Firebase configuration
+const app = initializeApp(firebaseConfig);
 const firebaseConfig = {
   apiKey: "AIzaSyAVA7vrkI_VNgQMLOM_glvRxYwJ05RnGzg",
   authDomain: "users-feaa8.firebaseapp.com",
