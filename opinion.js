c567c3c3df37ca5ac35269b78d0d9fa9f8596302
Sharon142 <culas.s@maristcollege.school.nz>
2021-11-08 10:57:15 +1300

    copying code from database

--- opinion.js
+++ opinion.js
@@ -39,18 +39,20 @@ db.collection('user').get().then(querySnapshot => {
         renderUser(doc);
     })
 })
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-import { initializeApp } from "firebase/app";
- "https://firebase.google.com/docs/web/setup#available-libraries.js"
- "https://www.gstatic.com/firebasejs/9.2.0/firebase-firestore.js"
-// Your web app's Firebase configuration
-const app = initializeApp(firebaseConfig);
-const firebaseConfig = {
-   apiKey: "AIzaSyAVA7vrkI_VNgQMLOM_glvRxYwJ05RnGzg",
-  authDomain: "users-feaa8.firebaseapp.com",
-  projectId: "users-feaa8",
-  storageBucket: "users-feaa8.appspot.com",
-  messagingSenderId: "606694220683",
-  appId: "1:606694220683:web:8ca0115dea4173eeb3c58e"
-};
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAVA7vrkI_VNgQMLOM_glvRxYwJ05RnGzg",
+    authDomain: "users-feaa8.firebaseapp.com",
+    projectId: "users-feaa8",
+    storageBucket: "users-feaa8.appspot.com",
+    messagingSenderId: "606694220683",
+    appId: "1:606694220683:web:8ca0115dea4173eeb3c58e"
+  };
 
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
