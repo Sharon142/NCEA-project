557c7868ba0a91e8658f49608a1ffe26c80210b1
Sharon142 <culas.s@maristcollege.school.nz>
2021-11-08 23:50:12 +1300

    adding nav bar

--- opinion.js
+++ opinion.js
@@ -1,3 +1,13 @@
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+  
 const modalWrapper = document.querySelector('.modal-wrapper');
 //modal add
 const addModal = document.querySelector ('.add-modal');
