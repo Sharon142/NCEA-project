11ea314c358bcf7c858629d260a66dbe7fb12523
Sharon142 <culas.s@maristcollege.school.nz>
2021-11-08 23:38:05 +1300

    adding js to nav bar

--- quiz.js
+++ quiz.js
@@ -1,3 +1,13 @@
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
 let userName = document.querySelector("#userName");
 let name = document.querySelector("#name");
 let greetings = document.querySelector("#greetings");
