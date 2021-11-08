6bde3164d45040130e48a2af30be386dcf5ad2b8
Sharon142 <culas.s@maristcollege.school.nz>
2021-11-08 23:42:40 +1300

    removing nav bar

--- quiz.js
+++ quiz.js
@@ -52,13 +52,5 @@ function nextQuestion() {
     document.body.style.backgroundColor = "darkturquoise";   
 }
 
-/* Open when someone clicks on the span element */
-function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-  }
-  
-  /* Close when someone clicks on the "x" symbol inside the overlay */
-  function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-  }
+
 
